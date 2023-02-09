74946a23b6241ccce8bdcbd711eade1a83d1ce09
HYSI-CILY <sicilyzhang409@gmail.com>
2023-02-09 11:06:21 +0100

    Comments for cl_model.py

--- cl_model.py
+++ cl_model.py
@@ -1,6 +1,4 @@
 import numpy as np
-import scipy
-import statistics
 import random
 
 # class of probability distribution
@@ -8,18 +6,20 @@ class Cl_distribution:
     def __init__(self, degree_sequence):
         self.mydegree_sequence = degree_sequence
 
+    # density funciton pi(i) = Di/2m
     def pdf(self, i):
         return self.mydegree_sequence[i]/np.sum(self.mydegree_sequence)
 
+    # cumulative density funciton
     def cdf(self):
         length = len(self.mydegree_sequence)
         p = [0]*length
-        print("helleo")
         p[0] = self.pdf(0)
         for  i in range (1,length):
             p[i] = p[i-1] + self.pdf(i)
         return p
 
+    # random value generator
     def rvs(self):
         u = random.uniform(0,1)
         p = self.cdf()
